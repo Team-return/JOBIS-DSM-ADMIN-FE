abf51cf5669c51520625297ebd2290bc84837b28
JJIIIINN <kgb21537@gmail.com>
2023-05-10 14:43:34 +0900

    fix: 코드 변경

--- src/apis/Applications/index.ts
+++ src/apis/Applications/index.ts
@@ -8,7 +8,7 @@ const router = '/applications';
 export const getApplicantInfo = async (applicationQueryString: ApplicantInfoQueryStringType) => {
 	const { application_status, student_name, company_id } = applicationQueryString;
 	const studentName = student_name ? `&student_name=${student_name}` : '';
-	const queryString = application_status || student_name || company_id ? `?application_status=${application_status}&company_id=${3}${studentName}` : '';
+	const queryString = application_status || student_name || company_id ? `?application_status=${application_status}&company_id=${company_id}${studentName}` : '';
 	const { data } = await instance.get<Promise<ApplicationResponse>>(`${router}${queryString}`);
 	return data;
 };
--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,8 +1,6 @@
 import axios, { AxiosError } from 'axios';
-import { Cookies } from 'react-cookie';
+import { useCookies } from 'react-cookie';
 import { reIssueToken } from './Auth';
-
-const cookie = new Cookies();
 
 export const instance = axios.create({
 	baseURL: process.env.REACT_APP_BASE_URL,
@@ -11,7 +9,8 @@ export const instance = axios.create({
 
 instance.interceptors.request.use(
 	(config) => {
-		const accessToken = cookie.get('access_token');
+		const [cookie] = useCookies();
+		const accessToken = cookie.access_token;
 		const returnConfig = {
 			...config,
 		};
@@ -28,26 +27,27 @@ instance.interceptors.response.use(
 	(error: AxiosError<AxiosError>) => {
 		if (axios.isAxiosError(error) && error.response) {
 			const { config } = error;
-			const refreshToken = cookie.get('refresh_token');
+			const [cookie, setCookie, removeCookie] = useCookies();
+			const refreshToken = cookie.refresh_token;
 			if (error.response.data.message === 'Invalid Token' || error.response.data.message === 'Token Expired') {
 				if (refreshToken) {
-					cookie.remove('access_token');
+					removeCookie('access_token');
 					reIssueToken(refreshToken)
 						.then((res) => {
 							const accessExpired = new Date(res.access_token_expired_at);
 							const refreshExpired = new Date(res.refresh_token_expired_at);
-							cookie.set('access_token', res.access_token, {
+							setCookie('access_token', res.access_token, {
 								expires: accessExpired,
 							});
-							cookie.set('refresh_token', res.refresh_token, {
+							setCookie('refresh_token', res.refresh_token, {
 								expires: refreshExpired,
 							});
 							if (config!.headers) config!.headers['Authorization'] = `Bearer ${res.access_token}`;
 							return axios(config!);
 						})
 						.catch(() => {
-							cookie.remove('access_token');
-							cookie.remove('refresh_token');
+							removeCookie('access_token');
+							removeCookie('refresh_token');
 							window.location.href = '/login';
 						});
 				} else {
