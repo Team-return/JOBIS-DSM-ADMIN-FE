ca7cec09ca9207e382bfc8f561b7ceb1d94c3781
JJIIIINN <kgb21537@gmail.com>
2023-12-04 09:51:24 +0900

    feat: zustand state 추가

---
+++ src/Store/State.ts
@@ -0,0 +1,48 @@
+import { ChangeEvent } from 'react';
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+import { ApplicantInfoQueryStringType } from '../Apis/Applications/request';
+
+interface applicationViewQueryStringState {
+	applicationViewQueryString: ApplicantInfoQueryStringType;
+	applicationViewQueryStringHandler: (
+		e: ChangeEvent<HTMLInputElement>
+	) => void;
+	applicationViewQueryStringDropDown: (name: string, value: any) => void;
+	setDefaultApplicationViewQueryString: () => void;
+}
+/** 선택한 카테코리 state */
+export const useApplicationViewQueryString =
+	create<applicationViewQueryStringState>()(
+		devtools((set) => ({
+			applicationViewQueryString: {
+				page: 1,
+				application_status: '',
+				student_name: '',
+				recruitment_id: '',
+			},
+			applicationViewQueryStringHandler: (e) =>
+				set((prev) => ({
+					applicationViewQueryString: {
+						...prev.applicationViewQueryString,
+						[e.target.name]: e.target.value,
+					},
+				})),
+			applicationViewQueryStringDropDown: (name, value) =>
+				set((prev) => ({
+					applicationViewQueryString: {
+						...prev.applicationViewQueryString,
+						[name]: value,
+					},
+				})),
+			setDefaultApplicationViewQueryString: () =>
+				set({
+					applicationViewQueryString: {
+						page: 1,
+						application_status: '',
+						student_name: '',
+						recruitment_id: '',
+					},
+				}),
+		}))
+	);
