eee30cb88924e15c52235b4b1c081c2383ae8749
은지 <wej0513@dsm.hs.kr>
2024-02-21 22:00:05 +0900

    feat::공지 작성 페이지 퍼블리싱 수정

--- src/Pages/BannerPage/styled.ts
+++ src/Pages/BannerPage/styled.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+	width: 100vw;
+	min-width: 1400px;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
--- src/Pages/CreateBannerPage/index.tsx
+++ src/Pages/CreateBannerPage/index.tsx
@@ -1,10 +1,11 @@
 import { CreateBanner } from '../../Components/CreateBanner';
 import { Header } from '../../Components/Header';
+import * as _ from './styled';
 export function CreateBannerPage() {
 	return (
-		<>
+		<_.Container>
 			<Header />
 			<CreateBanner />
-		</>
+		</_.Container>
 	);
 }
