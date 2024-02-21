333713650cc30a76aa3215b987f542c6471e3879
은지 <wej0513@dsm.hs.kr>
2024-02-21 21:29:06 +0900

    feat::공지 작성 페이지 퍼블리싱 수정

--- src/Pages/CreateBannerPage/index.tsx
+++ src/Pages/CreateBannerPage/index.tsx
@@ -1,6 +1,5 @@
 import { CreateBanner } from '../../Components/CreateBanner';
 import { Header } from '../../Components/Header';
-import * as _ from './styled';
 export function CreateBannerPage() {
 	return (
 		<>
