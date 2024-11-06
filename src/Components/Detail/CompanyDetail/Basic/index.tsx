36fcb2b1a47c3bd95faff0ee3ee9c54209d55bb8
은지 <wej0513@dsm.hs.kr>
2024-11-06 16:43:36 +0900

    feat :: 기업 상세 Input 삭제

--- src/Components/Detail/CompanyDetail/Basic/index.tsx
+++ src/Components/Detail/CompanyDetail/Basic/index.tsx
@@ -100,63 +100,32 @@ export function CompanyDetailBasic({
 				</_.ContentBox>
 			</_.Stack>
 			<_.Stack>
-				<_.TitleBox>본사주소</_.TitleBox>
+				<_.TitleBox>주소</_.TitleBox>
 				<_.ContentBox width={90}>
 					{`(${companyDetailInfo?.main_zip_code}) ${companyDetailInfo?.main_address} ${companyDetailInfo?.main_address_detail}`}
 				</_.ContentBox>
 			</_.Stack>
 			<_.Stack>
-				<_.TitleBox>지점주소</_.TitleBox>
-				<_.ContentBox width={90}>
-					{companyDetailInfo?.sub_address
-						? `(${companyDetailInfo?.sub_zip_code}) ${companyDetailInfo?.sub_address} ${companyDetailInfo?.sub_address_detail}`
-						: '-'}
-				</_.ContentBox>
-			</_.Stack>
-			<_.Stack>
-				<_.TitleBox>담당자1</_.TitleBox>
+				<_.TitleBox>담당자</_.TitleBox>
 				<_.ContentBox width={15}>
 					{companyDetailInfo?.manager_name}
 				</_.ContentBox>
-				<_.TitleBox>전화번호1</_.TitleBox>
+				<_.TitleBox>전화번호</_.TitleBox>
 				<_.ContentBox width={15}>
 					{companyDetailInfo?.manager_phone_no.replace(
 						/(\d{3})(\d{4})(\d{4})/,
 						'$1-$2-$3'
 					)}
 				</_.ContentBox>
-				<_.TitleBox>담당자2</_.TitleBox>
-				<_.ContentBox width={15}>
-					{companyDetailInfo?.sub_manager_name || '-'}
-				</_.ContentBox>
-				<_.TitleBox>전화번호2</_.TitleBox>
-				<_.ContentBox width={15}>
-					{companyDetailInfo?.sub_manager_phone_no
-						? companyDetailInfo?.sub_manager_phone_no.replace(
-								/(\d{3})(\d{4})(\d{4})/,
-								'$1-$2-$3'
-						  )
-						: '-'}
-				</_.ContentBox>
-			</_.Stack>
-			<_.Stack>
 				<_.TitleBox>이메일</_.TitleBox>
 				<_.ContentBox width={40}>
 					{companyDetailInfo?.email}
 				</_.ContentBox>
-				<_.TitleBox>팩스번호</_.TitleBox>
-				<_.ContentBox width={40}>
-					{companyDetailInfo?.fax || '-'}
-				</_.ContentBox>
 			</_.Stack>
 			<_.Stack>
-				<_.TitleBox>앱/웹 서비스명</_.TitleBox>
-				<_.ContentBox width={60}>
+				<_.TitleBox>대표 서비스명</_.TitleBox>
+				<_.ContentBox width={90}>
 					{companyDetailInfo?.service_name}
-				</_.ContentBox>
-				<_.TitleBox>사업분야</_.TitleBox>
-				<_.ContentBox width={20}>
-					{companyDetailInfo?.business_area}
 				</_.ContentBox>
 			</_.Stack>
 			<_.Stack>
